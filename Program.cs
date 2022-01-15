7352fc86e91005ab0f0b93e01000da33be1196cb
Icaro Nogueira <icaro.rodriguesti@gmail.com>
2022-01-15 13:27:46 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabuleiro;
+
+namespace Xadrez {
+
+    class Program {
+        static void Main (string [] args ){
+
+            Posicao p = new Posicao(1,2);
+
+            Console.WriteLine(p);
+            
+
+
+        }
+    }
+}
+
